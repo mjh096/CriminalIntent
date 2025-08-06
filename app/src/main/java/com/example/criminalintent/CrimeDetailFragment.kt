f3dabb753d0a916b1b5c8774a316af244ab5d645
Mitchell Harrington <mjh096@student.usc.edu.au>
2025-08-06 13:12:07 +1000

    Week 3 - Start CriminalIntent skeleton

---
+++ app/src/main/java/com/example/criminalintent/CrimeDetailFragment.kt
@@ -0,0 +1,38 @@
+package com.example.criminalintent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class CrimeDetailFragment : Fragment() {
+
+    private lateinit var crime: Crime
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_crime_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val titleField = view.findViewById<EditText>(R.id.crime_title)
+        val dateButton = view.findViewById<Button>(R.id.crime_date)
+        val solvedCheckBox = view.findViewById<CheckBox>(R.id.crime_solved)
+
+        titleField.setText(crime.title)
+        dateButton.text = crime.date.toString()
+        solvedCheckBox.isChecked = crime.isSolved
+    }
+}
